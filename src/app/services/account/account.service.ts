61a428dd39b2b5490ae0f43378be6127d91f5bb3
sergeyHudzenko <78405544+sergeyHudzenko@users.noreply.github.com>
2022-12-15 13:36:54 +0200

    1 file modified

--- src/app/services/account/account.service.ts
+++ src/app/services/account/account.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import accountData from '../../data/account/accountData';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+// import accountData from '../../data/account/accountData';
 import { IAccount } from '../../models/account/IAccount';
 import { LedgerTransactionType } from '../../models/ledger/ledger-transaction-type.enum';
 import { ErrorService } from '../error/error.service';
@@ -12,18 +12,47 @@ import { config } from '../../config/config';
   providedIn: 'root',
 })
 export class AccountService {
+  private accountData: Subject<{ data: IAccount }>;
+  private accountDataCached: Subject<{ data: IAccount }>;
+
   constructor(
     private errorService: ErrorService,
     private informationService: InformationService,
     private http: HttpClient
-  ) {}
+  ) {
+    this.initializeData();
+  }
+
+  refreshData() {
+    this.http
+      .get<{ data: IAccount[] }>(`${config.apiUrl}/user`)
+      .subscribe((res: any) => this.accountDataCached.next(res));
+  }
+
+  initializeData() {
+    if (!this.accountDataCached) {
+      this.accountDataCached = new Subject();
+      this.refreshData();
+    }
+  }
 
   get(): Observable<{ data: IAccount }> {
-    return this.http.get<{ data: IAccount }>(`${config.apiUrl}/user`);
+    this.accountDataCached.subscribe((data) => {
+      this.accountData.next(data);
+    });
+
+    return this.accountData;
   }
 
   set(amount: number) {
-    accountData.accountBalance = amount;
+    this.accountData.subscribe({
+      next: (v) => {
+        data: {
+          avatar: v.data.avatar;
+          accountBalance: amount;
+        }
+      },
+    });
   }
 
   compute(amount: number, type: LedgerTransactionType) {
